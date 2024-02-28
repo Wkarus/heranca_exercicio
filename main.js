879fbfe8c5b816686084538508a648790955e93d
Icaro Souza <icarololo13@gmail.com>
2024-02-28 17:34:27 -0300

    re-save

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 class Carro{
     constructor(nome,portas){
         this.nome = nome
-        this.portas =portas
+        this.portas = portas
         this.ligado = false
         this.vel =0
         this.cor=undefined
@@ -12,9 +12,10 @@ setCor=function(cor){
 }
 
 class Minivan extends Carro{
-constructor(nome,portas,espacoCarga){
+constructor(nome,portas,espacoCarga,qntPessoas){
 super(nome,portas)
-this.espacoCarga=espacoCarga
+this.espacoCarga = espacoCarga
+this.qntPessoas = qntPessoas
 this.setCor("preto")
 }
 
@@ -24,10 +25,19 @@ this.setCor("preto")
 
 const Carro1= new Carro ("Van",4)
 
-const c2=new Minivan("acura,"6)
+const Carro2=new Minivan("acura",6)
 
 console.log(`Nome: ${Carro1.nome}`)
 console.log(`portas: ${Carro1.portas}`)
 console.log(`ligado: ${Carro1.ligado}`)
 console.log(`vel: ${Carro1.vel}`)
 console.log(`cor: ${Carro1.cor}`)
+
+
+console.log(`Nome: ${Carro2.nome}`)
+console.log(`portas: ${Carro2.portas}`)
+console.log(`ligado: ${Carro2.ligado}`)
+console.log(`vel: ${Carro2.vel}`)
+console.log(`espacoCarga: ${Carro2.espacoCarga}`)
+console.log(`qntPessoas: ${Carro2.qntPessoas}`)
+console.log(`cor: ${Carro2.cor}`)
